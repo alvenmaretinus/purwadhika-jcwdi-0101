83b9a5bdd10469e87660ccc9fb06875a815623ec
alvenmaretinus <alvenmaretinus@gmail.com>
2025-07-23 18:53:55 +0700

    add FE session 5 code

---
+++ frontend-development/session 5/learn-hooks/src/app/hooks/useCounter.ts
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export interface UseCounterReturnValue {
+  count: number
+  increment: () => void
+  decrement: () => void
+}
+
+export default function useCounter(initialCount: number): UseCounterReturnValue {
+  const [_count, _setCount] = useState(initialCount || 0)
+
+  return {
+    count: _count,
+    increment: () => _setCount(count => count + 1),
+    decrement: () => _setCount(count => count - 2),
+  }
+}
